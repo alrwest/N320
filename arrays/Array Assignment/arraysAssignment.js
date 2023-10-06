4ed0fe83a4eae3dab8bcdea9ccf56c1757427716
alrwest <alrwest@iu.edu>
2023-10-06 02:29:09 -0400

    update array assignment

--- arrays/Array Assignment/arraysAssignment.js
+++ arrays/Array Assignment/arraysAssignment.js
@@ -1,4 +1,4 @@
-//student Array
+//students Array
 const students = [
 
     { name: 'Alice', grades: [85, 92, 78, 90] },
@@ -13,26 +13,21 @@ const students = [
    
     ];
 
-    //display names
-    const sList = document.getElementsByClassName("sList");
-
+    //forEach loop / display names
     students.forEach(student => {
-        let listElement = document.createElement('p');
-        listElement.textContent = student.name;
-        sList[0].appendChild(listElement);
+        const sList = document.getElementsByClassName("sList");
+        let studentList = document.createTextNode(student.name + " ");
+        sList[0].appendChild(studentList);
     });
 
-
-    //check to see if students passed using the every function
+    //check to see if every student passed 
     var allPass = students.every(students => {
         return students.grades.every(grade => grade >= 60);
     });
 
-    function displayPassingStudents() {
-        let pTag1 = document.getElementsByTagName("p")[7];
-        pTag1.textContent = allPass;
-    }
-    displayPassingStudents();
+    let passedTag = document.getElementsByTagName("p")[1];
+    let passedText = document.createTextNode(allPass);
+    passedTag.appendChild(passedText);
 
 
 //some method to see if at least one student has a perfect record
@@ -40,30 +35,28 @@ var perfectRecord = students.some(students => {
     let perfectGrade = 100;
     return students.grades.some(grade => grade === perfectGrade);
 });
-//console.log(perfectRecord);
 
-function displayPerfectRecord() {
-    let pTag9 = document.getElementsByTagName("p")[9];
-    pTag9.textContent = perfectRecord;
-}
-//displayPerfectRecord();
+//console.log(perfectRecord);
+let recordTag = document.getElementsByTagName("p")[2];
+let recordText = document.createTextNode(perfectRecord);
+recordTag.appendChild(recordText);
 
 
 //create a list of students with an average grade of 90+
-const goodGrades = () => {
-    let bestStudents = students.filter(function(student) {
-        let gradeSum = student.grades.reduce((accumulator, grade) => accumulator + grade, 0);
-        let average = gradeSum / student.grades.length;
-        return average >= 90;
+ const goodGrades = students.filter((student) => {
+            let gradeSum = student.grades.reduce((accumulator, grade) => accumulator + grade, 0);
+            let average = gradeSum / student.grades.length;
+            return average >= 90;
     });
 
-    //mapping out just the names of students
-    return bestStudents.map(student => student.name)
-}
-console.log("best students:", goodGrades());
+    //console.log("best students:", goodGrades);
+    let bestStudents = goodGrades.map(student => student.name);
 
-//mapping new array to show average of all students and their name in an unordered list 
+let bestStudentsTag = document.getElementsByTagName("p")[3];
+let bestStudentsText = document.createTextNode(bestStudents);
+bestStudentsTag.appendChild(bestStudentsText);
 
+//mapping new array to show average of all students
 const getStudentSummaries = () => {
     const studentSummaries = students.map(student => {
         let gradeSum = student.grades.reduce((accumulator, grade) => accumulator + grade, 0);
@@ -76,4 +69,26 @@ const getStudentSummaries = () => {
     return studentSummaries;
 }
 
-console.log(getStudentSummaries());
+//console.log(getStudentSummaries());
+let studentSummaries = getStudentSummaries();
+
+const displayAverages = (studentSummaries) => {
+    studentSummaries.forEach(value => {
+        let studentAvgs = document.getElementById('studentAvgs');
+        let listEl = document.createElement('li');
+        listEl.textContent = "Name: " + value.name + "," + " " + "Grade Average: " + value.average;
+        studentAvgs.appendChild(listEl);
+    });
+}
+displayAverages(studentSummaries);
+
+//using reduce to total # of calculate grades
+const totalNum = students.reduce((accumulator, student) => {
+    return accumulator + student.grades.length;
+}, 0);
+
+//console.log(totalNum);
+
+let totalNumPTag = document.getElementsByTagName('p')[5];
+let totalNumText = document.createTextNode(totalNum);
+totalNumPTag.appendChild(totalNumText);
